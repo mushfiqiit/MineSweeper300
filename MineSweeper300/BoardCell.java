b6d813fc08374910766d742b87e8528170631223
alecosmak <osmak@wisc.edu>
2020-07-07 13:19:45 -0500

    Add classes, method structure, and fields.

---
+++ MineSweeper300/BoardCell.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class BoardCell {
+
+   private final String COVER = "O";
+   private char value;
+   private boolean isShown;
+
+
+   BoardCell(int mineOdds) {
+      if (new Random().nextInt(mineOdds) == 0)
+         value = 'X';
+      else
+         value = '0';
+
+      isShown = false;
+   }
+
+
+   public char getValue() {
+      return value;
+   }
+
+   public boolean isShown() {
+      return isShown;
+   }
+
+
+   /**
+    * Determines what character should be printed based on if the value is shown
+    * or not.
+    * 
+    * @return A String of what should be printed when this cell is called to be
+    *         printed.
+    */
+   public String toString() {
+      if (isShown)
+         return Character.toString(value);
+      else
+         return COVER;
+   }
+
+
+}
---
+++ MineSweeper300/RunMineSweeper.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RunMineSweeper {
+
+   private final int[] MINEODDS = {10, 8, 6};
+   private final Scanner SCAN = new Scanner(System.in);
+   private ArrayList<ArrayList<BoardCell>> xList;
+   private ArrayList<BoardCell> yList;
+
+   
+   /**
+    * 
+    */
+   private static void printBoard() {
+
+   }
+
+   
+   /**
+    * 
+    * @param x X-coordinate of cell.
+    * @param y Y-coordinate of cell.
+    * @return
+    */
+   private static int countMines(int x, int y) {
+      
+      return 0;
+   }
+
+   
+   private static void createBoard() {
+      
+   }
+
+   
+   public static void main(String[] args) {
+      createBoard();
+      
+      printBoard();
+   }
+
+}
